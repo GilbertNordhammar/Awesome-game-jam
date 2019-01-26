d4793ee2f5576b344da981fe4b011db295e35dcd
GilbertNordhammar <gilbertnordhammar@gmail.com>
2019-01-26 22:59:01 +0100

    I'm god
    Created everything (aka the whole level design)

---
+++ The most awesome game jam game/Assets/SoundTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTest : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string soundEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var sound = FMODUnity.RuntimeManager.CreateInstance(soundEvent);
+        sound.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
